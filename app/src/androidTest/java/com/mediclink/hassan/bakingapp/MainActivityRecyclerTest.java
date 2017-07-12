ffcc912e4a0d7fbedc8c7bb7379b41d7d95ad923
Hilyas86 <htope68@gmail.com>
2017-07-12 01:42:39 +0100

    Add files to github

--- app/src/androidTest/java/com/mediclink/hassan/bakingapp/MainActivityRecyclerTest.java
+++ app/src/androidTest/java/com/mediclink/hassan/bakingapp/MainActivityRecyclerTest.java
@@ -2,13 +2,19 @@
 package com.mediclink.hassan.bakingapp;
 
 
-import android.support.test.espresso.contrib.RecyclerViewActions;
-import android.support.test.filters.LargeTest;
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 
 import com.mediclink.hassan.bakingapp.ui.MainActivity;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,9 +22,11 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
 
 
 /**
@@ -42,54 +50,53 @@ public class MainActivityRecyclerTest {
     public ActivityTestRule<MainActivity> mActivityTestRule =
             new ActivityTestRule<>(MainActivity.class);
 
-    @Test
-    public void checkifRecyclerisPresent() {
+//    @Test
+//    public void checkifRecyclerisPresent() {
+//
 
-
-        onView(withId(R.id.recipeRecycler)).check(matches(hasDescendant(withText("Brownies"))));
-        onView(withId(R.id.recipeRecycler)).perform(RecyclerViewActions.actionOnItemAtPosition(2, click()));
+    //    onView(withId(R.id.recipeRecycler)).check(matches(hasDescendant(withText("Brownies"))));
+  //      onView(withId(R.id.recipeRecycler)).perform(RecyclerViewActions.actionOnItemAtPosition(2, click()));
 //               .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
 //
 //
 //    }
 
 
-//    private static Matcher<View> childAtPosition(
-//            final Matcher<View> parentMatcher, final int position) {
-//
-//        return new TypeSafeMatcher<View>() {
-//            @Override
-//            public void describeTo(Description description) {
-//                description.appendText("Child at position " + position  + " in parent ");
-//                parentMatcher.describeTo(description);
-//            }
-//
-//            @Override
-//            public boolean matchesSafely(View view) {
-//                ViewParent parent = view.getParent();
-//                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-//                        && view.equals(((ViewGroup) parent).getChildAt(position ));
-//            }
-//        };
-//    }
-//
-//    @Test
-//    public void mainActivityTest() {
-//        ViewInteraction textView = onView(
-//                allOf(withId(R.id.recipeName), withText("Nutella Pie"),
-//                        childAtPosition(
-//                                childAtPosition(
-//                                        withId(R.id.recipeRecycler),
-//                                        0),
-//                                0),
-//                        isDisplayed()));
-//        textView.check(matches(withText("Nutella Pie")));
-//
-//        ViewInteraction recyclerView = onView(
-//                allOf(withId(R.id.recipeRecycler), isDisplayed()));
-//        recyclerView.perform(actionOnItemAtPosition(0, click()));
-//
-//    }
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position  + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position ));
+            }
+        };
+    }
+
+    @Test
+    public void mainActivityTest() {
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.recipeName), withText("Nutella Pie"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.recipeRecycler),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textView.check(matches(withText("Nutella Pie")));
+
+        ViewInteraction recyclerView = onView(
+                allOf(withId(R.id.recipeRecycler), isDisplayed()));
+        recyclerView.perform(actionOnItemAtPosition(0, click()));
 
     }
-}
+
+    }
